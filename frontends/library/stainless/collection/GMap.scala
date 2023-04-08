c6c5a34b93841129a3f9d888f6174a1677d01c13
@DerouichAbdessalam <abdessalam.derouich@epfl.ch>
2023-04-08 17:52:02 +0200

    add correct set implem

--- frontends/library/stainless/collection/GMap.scala
+++ frontends/library/stainless/collection/GMap.scala
@@ -6,34 +6,54 @@ import stainless.lang._
 import stainless.annotation._
 
   /**
-    * defining a map item 
-    *
-    * @param value
-    * @param present
-    */
-  case class MapValue[B](value : B ,present : Boolean)
+  * defining a map item 
+  *
+  * @param value value of the map item
+  * @param present indicates presence of the item in the map 
+  */
+case class MapValue[B](value : B ,present : Boolean)
+
+/**
+  * state of the Gmap 
+  *
+  * @param knownItems the items encountered by the map (either present or absent)
+  * @param unknownItemInvariant invariant that should hold on the unknown item if present 
+  * @param length number of known present elements in the map
+*/
+case class MapState[K, V](knownItems: Map[K,MapValue[V]], unknownItemInvariant : (K, MapValue[V]) => Boolean, length : BigInt)
 
 
-  case class MapState[A, B](knownItems: Map[A,MapValue[B]], unknownItemInvariant : MapValue[B] => Boolean, length : BigInt)
-
-
-
-class GMap[A, B](uknownItem : (Key, MapValue[B]), unknownItemInvariantInit : (Key, MapValue[B]) => Boolean ){
+/**
+  * Ghost map class implementation
+  *
+  * @param uknownItem the special item representing all items not yet known by the map 
+  * @param unknownItemInvariant the  unknown item invariant represents the condition that all items not yet known should 
+  *     satisfy if they're present in our map  
+  */
+class GMap[A, B](uknownItem : (Key, MapValue[B]), unknownItemInvariant : (Key, MapValue[B]) => Boolean){
   
   type Key = A
   type Value = B
   
   //the map state 
-  var MapState = MapState[B](Map.Empty[Key, MapValue[B]], unknownItemInvariantInit, 0)
+  var mapState = MapState[B](Map.Empty[Key, MapValue[B]], unknownItemInvariant, 0)
   
-
+  /**
+    * tries to find the value associated with the key given as argument, 
+    * if the key corresponds to a known item the value, presence pair 
+    * returned will match the one found. otherwise we return a value, presence pair
+    * verifying together with the key the uknown item invariant
+    *
+    * @param key 
+    * @returnthe value, presence bollean pair
+    */
   def get(key: Key): (Value, Boolean) = {
-    MapState.knownItems().get(key) match {
+    mapState.knownItems().get(key) match {
       case None => {
         // we generate the two fresh values 
         // such that the unkown item invariant applies on the 2 values 
-        // MapState.unknownItemInvariant(key, freshV, freshP)
-        choose[(Value, Boolean)](t => MapState.unknownItemInvariant(key, MapValue(t._1, t._2)))
+        // mapState.unknownItemInvariant(key, freshV, freshP)
+        choose[(Value, Boolean)](t => mapState.unknownItemInvariant(key, MapValue(t._1, t._2)))
       }
       case Some(mapValue) => {
         // we generate the two fresh values 
@@ -45,61 +65,73 @@ class GMap[A, B](uknownItem : (Key, MapValue[B]), unknownItemInvariantInit : (Ke
   }ensuring{
     case (value, present) => 
       // Check that the number of unique known items does not exceed its length and the invariants 
-      MapState.knownItems.filter{case MapItem(_,_,p) => p}.size <= MapState.length
+      mapState.knownItems.filter{case MapItem(_,_,p) => p}.size <= mapState.length
 
   }
 
   /**
-    * set method for GMAP class 
+    * creates a new map where the key->value mapping is added 
+    *
+    * @param key
+    * @param value
+    * @return the map with the added mapping 
     */
   def set(key: Key, value: Value): GMap[A, B] = {
+
+    //checking if the element was already present in the map
     val (oldValue, present) = get(key)
-    val newLength = if (present) MapState.length else MapState.length + 1
-    
-    //no need to create a new state : 
-    val newKnownItems = MapState.knownItems.map {
-      //case the value was already in the map
+    val newLength = if (present) mapState.length else mapState.length + 1
+
+    //no need to create a new state, we just map the old one : 
+    val newKnownItems = mapState.knownItems.map {
+      //case we find the key in the map
       case (k, MapValue(v, p)) if k == key =>
         k -> MapValue(value, true)
-      case (k, MapValue(v, p)) =>
-        k -> MapValue(v, p)
+      //case the key is different from the key to set  
+      case x => x
     } + (key -> MapValue(value, true))
-    // this last addition is made to add the eleent to the list of known elems if 
-    //it wasn't previously there    
+    //this last addition is done in case the key wasn't present in the map 
+    
+     
+    val newMap = GMap(uknownItem, unknownItemInvariant)
+    newMap.mapState = MapState(newKnownItems, unknownItemInvariant, newLength)
+    
+  }ensuring{newMap => 
 
-    GMap(uknownItem, unknownItemInvariantInit).ensuring { map =>
-      map.MapState.knownItems.keySet == newKnownItems.keySet &&
-      newKnownItems.forall {
-        case (k, v) => 
-          MapState.knownItems.get(k) match {
-            case Some(MapValue(value, present)) =>
-              (value == v.value && present == v.present) || k == key
-            case None =>
-              MapState.unknownItemInvariant(v)
-          }
-      } &&
-      map.MapState.length == newLength &&
-      map.getItemInvariants == MapState.unknownItemInvariant
-    }
+    //ensure the semantical link between the elements of the [pre-set map]<->[post-set map]     
+    forall((keyPrime: Key) => (key != keyPrime) ==> (get(map, keyPrime) == get(newMap, keyPrime)))
   }
 
+
+
   /**
-    * remove method implementation 
+    * creates a new map where the key->value mapping is removed if it was previously present
+    *
+    * @param key
+    * @param value
+    * @return the map with the removed mapping 
     */
   def remove(key: Key): GMap[A, B] = {
+
+    //checking if the element was already present in the map
     val (value, present) = get(key)
-    val newLength = MapState.length + (if (present) -1 else 0)
-    val newKnownItems = MapState.knownItems.map{ case (k, MapValue(v, p)) =>
-      if (k == key) (k, MapValue(value, false))
-      else (k, MapValue(v, p && k != key))
-    } + (key -> MapValue(value, false))
-    GMap[A, B](MapValue[B](value, false), MapState.unknownItemInvariant) ensuring { gmap =>
-      gmap.MapState.length == newLength &&
-      gmap.MapState.knownItems.keySet == newKnownItems.keySet &&
-      gmap.MapState.knownItems.forall{ case (k, MapValue(v, p)) =>
+    val newLength = mapState.length + (if (present) -1 else 0)
+
+    val newKnownItems = mapState.knownItems.map{ 
+      //case we encounter the key : associate the key with a fresh value and false presence
+      case (k, MapValue(v, p)) if (k == key)  =>
+        k -> MapValue(choose[Value], false) 
+      //otherwise keep the element intact 
+      case x => x
+    } +(k -> MapValue(choose[Value], false)) 
+
+    GMap[A, B](MapValue[B](value, false), mapState.unknownItemInvariant) ensuring { gmap =>
+      gmap.mapState.length == newLength &&
+      gmap.mapState.knownItems.keySet == newKnownItems.keySet &&
+      gmap.mapState.knownItems.forall{ case (k, MapValue(v, p)) =>
         newKnownItems.get(k).contains(MapValue(v, p))
       } &&
-      getItemInvariants(gmap.MapState.unknownItemInvariant)
+      getItemInvariants(gmap.mapState.unknownItemInvariant)
     }
   }
 
@@ -144,7 +176,7 @@ class GMap[A, B](uknownItem : (Key, MapValue[B]), unknownItemInvariantInit : (Ke
 
 
 
-}
+
 
     
 
