56880d39d6f542b3e6fb263c0bab054a7d15725c
abdessalam derouich <abdessalam.derouich@epfl.ch>
2023-04-30 00:09:15 +0200

    added positive and negative forAll test, still buggy

--- TestsStainlessAbdess/Tests_stainless/src/ArrayAsMap.scala
+++ TestsStainlessAbdess/Tests_stainless/src/ArrayAsMap.scala
@@ -2,12 +2,12 @@ import stainless.collection._
 import stainless.lang._
 import stainless.annotation._
 
-//we define an array only containing positive elements only 
+//we define an array implemented as a map
 object ArrayAsMap {
 
-    def arrayInvariant(key : Int, mapValue: MapValue[Int]): Boolean = mapValue match 
-        case MapValue(value, presence) =>  value >= 0 
-    
+    // the elements not yet in the ß
+    def arrayInvariant(key : Int, mapValue: MapValue[Int]): Boolean = mapValue.value >= 0
+
     //what value to choose for the unnown item (does any value verifying the invarisnt work ?)
     //the unknown item is only supposed to contain values that i'll add to it, no reason to think about absent elements
     def unknownItem : (Int, MapValue[Int]) = (-1 , MapValue(1, false))
@@ -22,6 +22,18 @@ object ArrayAsMap {
         val arrayMap : GMap[Int, Int] = GMap(unknownItem, arrayInvariant) 
         val nA = arrayMap.set(0, 1)
         nA.get(0)
-        nA.getPost(0)        
-              
+        nA.getPost(0)     
+
+    def positiveMapForAllOp() : Boolean = {
+        val arrayMap : GMap[Int, Int] = GMap(unknownItem, arrayInvariant) 
+        val nA = arrayMap.set(0, 1)
+        nA.forAll((x: Int, mapV : MapValue[Int]) => mapV.value >= 0)
+    }ensuring(res => res)
+
+
+    def negativeMapForAllOp() : Boolean = {
+        val arrayMap : GMap[Int, Int] = GMap(unknownItem, arrayInvariant) 
+        val nA = arrayMap.set(0, -1)
+        nA.forAll((x: Int, mapV : MapValue[Int]) => mapV.value >= 0)
+    }ensuring(res => !res)
 }
